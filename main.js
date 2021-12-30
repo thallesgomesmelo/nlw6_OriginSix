abe0e5a07ed2b1dbe1b2dc5261b182cc040c23c6
unknown <thallesgomesmello@gmail.com>
2021-12-30 10:38:08 -0300

    Feito tela de depoimento usando swiper.

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-    element.addEventListener('click',() => {
-        nav.classList.toggle('show')
-    })
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show')
+  })
 }
 
 /* Quando clicar em um item do menu, esconde o menu. */
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
-    link.addEventListener('click', () => {
-        nav.classList.remove('show')
-    })
+  link.addEventListener('click', () => {
+    nav.classList.remove('show')
+  })
 }
 
 /* Adiciona  sombra quando de scroll na página. */
@@ -22,11 +22,21 @@ const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
 window.addEventListener('scroll', () => {
-    if(window.scrollY >= navHeight) {
-        //scroll é maior que a altura do header
-        header.classList.add('scroll')
-    } else {
-        //nemor que a altura do header
-        header.classList.remove('scroll')
-    }
+  if (window.scrollY >= navHeight) {
+    //scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //nemor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
+
+/* Testimonial swipper */
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
 })
